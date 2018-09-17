70bf38d93c3a8bb55f56a8da72eaef6e13fa7688
Justina <Justez@users.noreply.github.com>
2018-09-17 08:52:02 +0300

    Navigation update

--- javascript/components/Header.js
+++ javascript/components/Header.js
@@ -1,48 +1,48 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
 import { displayName } from '../../app'
 import { RkTheme, RkText, RkTabView } from 'react-native-ui-kitten'
+
+const dimensions = Dimensions.get('window').width < 400
 
 const Header = () =>
   <View>
     <RkText rkType='basicHeader'>{displayName}</RkText>
     <View style={styles.navigation}>
-      <RkTabView rkType='dark nav' onTabChanged={(index) => console.warn(index)}>
-        <RkTabView.Tab title={'Home'}/>
+      <RkTabView rkType='material nav' onTabChanged={(index) => console.warn(index)}>
+        <RkTabView.Tab title={'Search'}/>
+        <RkTabView.Tab title={'Favourites'}/>
         <RkTabView.Tab title={'History'}/>
-        <RkTabView.Tab title={'Other'}/>
       </RkTabView>
     </View>
   </View>
 
 RkTheme.setType('RkText', 'basicHeader', {
   alignSelf: 'center',
-  fontSize: 40,
+  fontSize: dimensions ? 30 : 45
 })
 
-RkTheme.setType('RkTabView', 'dark', {
-  backgroundColor:'#778f9b',
-  color:'white',
-  borderColor:'#4a636d'
+RkTheme.setType('RkTabView', 'material', {
+  color:'black',
+  borderColor:'transparent',
 })
 
- RkTheme.setType('RkTabView', 'darkSelected', {
-  backgroundColor:'#4a636d',
-  borderColor:'#4a636d'
+ RkTheme.setType('RkTabView', 'materialSelected', {
+  color:'black',
 })
 
 RkTheme.setType('RkTabView', 'nav', {
-  headerContainer: {
-
-  },
+  backgroundColor: 'transparent',
   tabContainer: {
-
+    margin: 0,
+    borderWidth: 0,
+    paddingBottom: 10,
+    paddingTop: dimensions ? 8 : 12
   },
   content: {
     fontFamily: 'serif',
-    fontSize: 15,
-    paddingBottom: 6,
-    paddingTop: 6,
+    fontSize: dimensions ? 14 : 18,
+    padding: 0,
   }
 })
 
@@ -54,7 +54,6 @@ RkTheme.setType('RkText', 'nav', {
 const styles = StyleSheet.create({
   navigation: {
     flexDirection: 'row',
-    backgroundColor: 'black',
     width: '100%'
   }
 })
